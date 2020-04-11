377a187747d2b7fb766befe39d3b0e4aed7bacec
Pedrodotcom <pedroluisaantonio@gmail.com>
2020-04-11 15:07:20 -0300

    Início da versão 'mobile' da aplicação

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import logoImg from '../../assets-app-mobile/logo.png'
+
+import styles from './styles'
+
+export default function Incidents () {
+    return (
+        <View style={styles.container}>
+            <Text>
+                
+            </Text>
+        </View>
+    )
+}
